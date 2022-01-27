5a376bea6d436f96c67e0179a6ed5545c0748cb0
MessiasFilho <88682777+MessiasFilho@users.noreply.github.com>
2022-01-27 00:02:13 -0300

    Create SellerDAOJDBC.java

---
+++ src/Model/DAO/IMP/SellerDAOJDBC.java
@@ -0,0 +1,39 @@
+package Model.DAO.IMP;
+
+import java.util.List;
+
+import Model.DAO.SellerDAO;
+import Model.Entities.Seller;
+
+public class SellerDAOJDBC implements SellerDAO {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DeletById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findALL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
