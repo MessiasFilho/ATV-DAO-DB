e6da8830b04ca10a3cadec34e72b4e410d30a6f8
MessiasFilho <88682777+MessiasFilho@users.noreply.github.com>
2022-05-31 22:43:56 -0300

    Update DaoFactory.java

--- src/Model/DAO/DaoFactory.java
+++ src/Model/DAO/DaoFactory.java
@@ -1,11 +1,12 @@
 package Model.DAO;
 
+import DB.Conecxao;
 import Model.DAO.IMP.SellerDAOJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDAO CreateSellerDao() {
-		return new SellerDAOJDBC() ;  
+		return new SellerDAOJDBC(Conecxao.getConnection() ) ;  
 	}
 
 	
